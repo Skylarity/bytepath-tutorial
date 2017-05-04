94f25832fd53e4c2e0a085696a1f4b111825d4ba
Skyler Rexroad <skyrex1095@gmail.com>
2017-05-04 13:42:33 -0500

    Reorganized project structure

---
+++ helpers/helpers.lua
@@ -0,0 +1,51 @@
+--[[ LOADING ]]--
+
+function recursiveEnumerate(folder, file_list)
+    local items = love.filesystem.getDirectoryItems(folder)
+
+    for _, item in ipairs(items) do
+        local file = folder .. '/' .. item
+
+        if love.filesystem.isFile(file) then
+            table.insert(file_list, file)
+        elseif love.filesystem.isDirectory(file) then
+            recursiveEnumerate(file, file_list)
+        end
+    end
+end
+
+function requireFiles(files)
+    for _, file in ipairs(files) do
+        local file = file:sub(1, -5)
+        require(file)
+    end
+end
+
+--[[ ROOMS ]]--
+
+function addRoom(room_type, room_name, ...)
+    local room = _G[room_type](room_name, ...)
+    rooms[room_name] = room
+    return room
+end
+
+function gotoRoom(room_type, room_name, ...)
+    if current_room and rooms[room_name] then
+        if current_room.deactivate then current_room:deactivate() end
+        current_room = rooms[room_name]
+        if current_room.activate then current_room:activate() end
+    else
+        current_room = addRoom(room_type, room_name, ...)
+    end
+end
+
+--[[ GENERAL ]]--
+
+function UUID()
+    local fn = function(x)
+        local r = math.random(16) - 1
+        r = (x == "x") and (r + 1) or (r % 4) + 9
+        return ("0123456789abcdef"):sub(r, r)
+    end
+    return (("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"):gsub("[xy]", fn))
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+require 'helpers/helpers'
 Object = require 'libraries/classic/classic'
 Input = require 'libraries/boipushy/Input'
 Timer = require 'libraries/enhanced_timer/EnhancedTimer'
@@ -30,45 +31,4 @@ function love.draw()
 	if current_room then current_room:draw() end
 
 	--
-end
-
---[[ HELPERS ]]--
-
-function addRoom(room_type, room_name, ...)
-	local room = _G[room_type](room_name, ...)
-	rooms[room_name] = room
-	return room
-end
-
-function gotoRoom(room_type, room_name, ...)
-	if current_room and rooms[room_name] then
-		if current_room.deactivate then current_room:deactivate() end
-		current_room = rooms[room_name]
-		if current_room.activate then current_room:activate() end
-	else
-		current_room = addRoom(room_type, room_name, ...)
-	end
-end
-
---[[ LOADING ]]--
-
-function recursiveEnumerate(folder, file_list)
-	local items = love.filesystem.getDirectoryItems(folder)
-
-	for _, item in ipairs(items) do
-		local file = folder .. '/' .. item
-
-		if love.filesystem.isFile(file) then
-			table.insert(file_list, file)
-		elseif love.filesystem.isDirectory(file) then
-			recursiveEnumerate(file, file_list)
-		end
-	end
-end
-
-function requireFiles(files)
-	for _, file in ipairs(files) do
-		local file = file:sub(1, -5)
-		require(file)
-	end
 end
