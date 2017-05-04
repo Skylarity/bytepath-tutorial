74624ed3349bed7f9b271cd5f3c24632b222e1ed
Skyler Rexroad <skyrex1095@gmail.com>
2017-05-04 13:43:43 -0500

    Unborked

--- objects/GameObject.lua
+++ objects/GameObject.lua
@@ -2,7 +2,7 @@ GameObject = Object:extend()
 
 function GameObject:new(area, x, y, opts)
     local opts = opts or {}
-    if opts then for k, v, in pairs(opts) do self[k] = v end end
+    if opts then for k, v in pairs(opts) do self[k] = v end end
 
     self.area = area
     self.x, self.y = x, y
