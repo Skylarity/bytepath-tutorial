09a5ebc09f450ce6c60b9bee61269aa3b6b4cf8d
Skyler Rexroad <skyrex1095@gmail.com>
2017-05-04 13:57:21 -0500

    Updated Area - can add objects to area now

--- rooms/Area.lua
+++ rooms/Area.lua
@@ -6,6 +6,7 @@ function Area:new(room)
 end
 
 function Area:update(dt)
+    -- Loop backwards to account for weird Lua table stuff
     for i = #self.game_objects, 1, -1 do
         local game_object = self.game_objects[i]
         game_object:update(dt)
@@ -15,4 +16,11 @@ end
 
 function Area:draw(dt)
     for _, game_object in ipairs(self.game_objects) do game_object:draw(dt) end
+end
+
+function Area:addGameObject(game_object_type, x, y, opts)
+    local opts = opts or {}
+    local game_object = _G[game_object_type](self, x or 0, y or 0, opts)
+    table.insert(self.game_objects, game_object)
+    return game_object
 end
